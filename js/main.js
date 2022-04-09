1de91ce0a52375671d6ee743e7254f3599c64a54
romartiny <91370772+romartiny@users.noreply.github.com>
2022-04-10 01:05:34 +0300

    add js

---
+++ js/main.js
@@ -0,0 +1,51 @@
+var timeDisplay = document.getElementById("time");
+
+
+function refreshTime() {
+    let today = new Date();
+    switch(new Date().getMonth()) {
+        case 1:
+            cmonth = 'January';
+            break;
+        case 2:
+            cmonth = 'February';
+            break;
+        case 3:
+            cmonth = "March";
+            break;
+        case 4:
+            cmonth = 'April';
+            break;
+        case 5:
+            cmonth = 'May';
+            break;
+        case 6:
+            cmonth = 'June';
+            break;
+        case 7:
+            cmonth = 'July';
+            break;
+        case 8:
+            cmonth = 'August';
+            break;
+        case 9:
+            cmonth = 'September';
+            break;
+        case 10:
+            cmonth = 'October';
+            break;
+        case 11:
+            cmonth = 'November';
+            break;
+        case 12:
+            cmonth = 'December';
+            break;
+        default:
+            cmonth = "You have 13 month year?";
+    }
+    document.getElementById('time').innerHTML = today.getHours() + ":" +  today.getMinutes();
+    document.getElementById('date').innerHTML = today.getDate() + ' ' + cmonth;
+
+}
+
+setInterval(refreshTime, 1000);
